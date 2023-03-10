04c206fdd81a6ffb11d92733dea4c26789f9d827
João Pedro Pereira Lima <84398388+JoaoPedroSH@users.noreply.github.com>
2023-03-10 15:11:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-/* Requisições */
 const getAds = async () => {
   const response = await fetch("http://localhost:3300/html");
   const json = await response.json();
   console.log(json);
-
-  /* Pega o html retornado na requisição e adiciona na div especificada */
   const htmlDiv = document.getElementById("upeeads__html");
   htmlDiv.innerHTML = json.html;
 };
-getAds();
 
 const postInterval = async () => {
   const data = { cod: 0 };
@@ -51,45 +47,54 @@ const putClosePage = async () => {
   console.log(json);
 };
 
+const screenDimensions = () => {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  console.log(`A largura da tela é: ${screenWidth}`);
+  console.log(`A altura da tela é: ${screenHeight}`);
+};
+
+const observerDiv = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const divObservada = document.querySelector("#upeeads__html");
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    };
+    let visualized = false;
+    if (visualized == false) {
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let segundos = 0;
+            const contador = setInterval(() => {
+              segundos++;
+              if (segundos >= 5) {
+                clearInterval(contador);
+                if (entry.intersectionRatio >= 0.5) {
+                  console.log(
+                    "Div visualizada por 5 segundos. Fazendo a requisição..."
+                  );
+                  postAdsVisualized();
+                  visualized = true;
+                }
+              }
+            }, 1000);
+          }
+        });
+      }, options);
+      observer.observe(divObservada);
+    }
+  });
+};
+
+
+/* Requisição que pega o html retornado e adiciona na div especificada */
+getAds();
+/* Pega as dimensões da tela */
+screenDimensions();
+/* Observa se o usuário está visualizando uma div especifica de forma completa e se permanecer por 5 segundos, é enviada uma requisição */
+observerDiv();
 /* Realiza requisições de tempos em tempos */
 setInterval(postInterval, 6000);
-
-/* Pega as dimensões da tela */
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-console.log(`A largura da tela é: ${screenWidth}`);
-console.log(`A altura da tela é: ${screenHeight}`);
-
-/* Observa se o usuário está visualizando uma div especifica de forma completa e se permanecer por 5 segundos, é enviada uma requisição */
-document.addEventListener("DOMContentLoaded", () => {
-  const divObservada = document.querySelector("#upeeads__html");
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1,
-  };
-  let visualized = false;
-  if (visualized == false) {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          let segundos = 0;
-          const contador = setInterval(() => {
-            segundos++;
-            if (segundos >= 5) {
-              clearInterval(contador);
-              if (entry.intersectionRatio >= 0.5) {
-                console.log(
-                  "Div visualizada por 5 segundos. Fazendo a requisição..."
-                );
-                postAdsVisualized();
-                visualized = true;
-              }
-            }
-          }, 1000);
-        }
-      });
-    }, options);
-    observer.observe(divObservada);
-  }
-});
